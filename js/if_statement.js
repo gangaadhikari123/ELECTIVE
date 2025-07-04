c1031153937c9210c32d4ccafbc80f2bb2159d29
Ganga Adhikari <adhikariganga2061@gmail.com>
2025-07-04 09:35:17 +0545

    Fetch data from an API and render it:

---
+++ js/if_statement.js
@@ -0,0 +1,11 @@
+const age = 15;
+if (age >= 18) {
+    console.log("Adult");
+  } else if (age >= 13) {
+    console.log("Teen");
+  } else {
+    console.log("Child");
+  }
+  
+  // Ternary operator
+  const status = age >= 18 ? "Adult" : "Minor";
